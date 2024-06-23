86a0e84346972017beec90bb45d1ab95e87d6fee
ylle014 <katsukiyu38@gmail.com>
2024-06-23 20:15:14 +0800

    kolo removed2

--- AgoraProj/AgoraProj/settings.py
+++ AgoraProj/AgoraProj/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'kolo.middleware.KoloMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,8 +54,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
-    
-    
 ]
 
 
