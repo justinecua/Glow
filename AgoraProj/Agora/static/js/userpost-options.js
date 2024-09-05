5693fcad3efc0613a30dda166c73dd97d345c9b1
justinecua <katsukiyu38@gmail.com>
2024-09-05 17:39:38 +0800

    update database

--- AgoraProj/Agora/static/js/userpost-options.js
+++ AgoraProj/Agora/static/js/userpost-options.js
@@ -1,6 +1,7 @@
 let PostOptions = document.querySelectorAll('.Post-Options2');
 let UserPostOptionsP = document.querySelectorAll('.UserPost-Options');
 let DeletePostOverlay = document.getElementById('DeletePost-Overlay');
+let EditPostOverlay = document.getElementById('EditPost-Overlay');
 let CloseDeletePost = document.getElementById('Close-DeletePost');
 let DPBack = document.getElementById('DP-Back');
 let DPSubmit = document.querySelector('.DP-Submit');
@@ -29,12 +30,19 @@ PostOptions.forEach(options => {
     postId = postContainer.getAttribute('data-postid');
     let userPostOptions = postContainer.querySelector('.UserPost-Options');
     let PostDeleteBtn = postContainer.querySelector('.Post-DeleteBtn');
+    let PostEditBtn = document.querySelector('.Post-EditBtn');
 
     PostDeleteBtn.addEventListener('click', function() {
       DeletePostOverlay.style.display = 'flex';
       userPostOptions.style.display = "none";
       console.log(postId);
     });
+
+    PostEditBtn.addEventListener('click', function(){
+      EditPostOverlay.style.display= "flex";
+      userPostOptions.style.display = "none";
+      console.log(postId);
+    })
 
     userPostOptions.style.display = "flex";
   });
